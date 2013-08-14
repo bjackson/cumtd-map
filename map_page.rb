e3804611aa66e4b21b8643d3c90d27f910f2d3a6
Brett Jackson <brett@brettjackson.org>
2013-08-14 02:38:35 -0500

    remove sinatra asssets

--- map_page.rb
+++ map_page.rb
@@ -5,35 +5,6 @@ require 'rufus-scheduler'
 require 'yaml'
 require 'colorize'
 
-require 'sinatra/base'
-require 'sinatra/assetpack'
-
-class App < Sinatra::Base
-  set :root, File.dirname(__FILE__) # You must set app root
-
-  register Sinatra::AssetPack
-
-  assets {
-    serve '/js',     from: 'app/js'        # Default
-    serve '/css',    from: 'app/css'       # Default
-    serve '/images', from: 'app/images'    # Default
-    serve '/fonts',  from: 'app/fonts'
-
-    # The second parameter defines where the compressed version will be served.
-    # (Note: that parameter is optional, AssetPack will figure it out.)
-    js :app, '/js/app.js', [
-      '/js/vendor/**/*.js',
-      '/js/lib/**/*.js'
-    ]
-
-    css :application, '/css/application.css', [
-      '/css/screen.css'
-    ]
-
-    js_compression  :jsmin    # :jsmin | :yui | :closure | :uglify
-    css_compression :simple   # :simple | :sass | :yui | :sqwish
-  }
-end
 
 
 
